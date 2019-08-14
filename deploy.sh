194b79f8f269109cd450e1cd8c871383af4431a0
Alex Bogdanovski <alex@erudika.com>
2019-08-15 00:02:13 +0300

    updated demo

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 rm -rf docs
 BASE_HREF=/react-para npm run build
+sed -i -e "s/\"homepage\": \"\\.\"/\"homepage\": \"https:\\/\\/albogdano\\.github\\.io\\/react-para\"/g" package.json
 sed -i -e "s/\"start_url\": \"\\.\"/\"start_url\": \"\\/react-para\\/\"/g" build/manifest.json
 mv build docs
 sed -i -e "s/\"homepage\": .*$/\"homepage\": \"\\.\",/g" package.json
