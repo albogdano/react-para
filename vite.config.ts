32bdc1fb3939f953ac4bf58d1f14861fa4b8c18c
Alex Bogdanovski <alex@erudika.com>
2025-07-01 16:09:05 +0300

    updated demo

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: process.env.PUBLIC_PATH || '/react-para/',
   plugins: [reactRouter(), tsconfigPaths()],
 });
