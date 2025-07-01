b03ad0ec7697f731ae693b1f48e126d7f8c459e9
Alex Bogdanovski <alex@erudika.com>
2025-07-01 15:33:35 +0300

    updated demo

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 rm -rf docs
-sed -i -e "s/\"homepage\": \"\\.\"/\"homepage\": \"https:\\/\\/albogdano\\.github\\.io\\/react-para\"/g" package.json
+#sed -i -e "s/\"homepage\": \"\\.\"/\"homepage\": \"https:\\/\\/albogdano\\.github\\.io\\/react-para\"/g" package.json
 npm run build
-sed -i -e "s/\"start_url\": \"\\.\"/\"start_url\": \"\\/react-para\\/\"/g" build/manifest.json
-mv build docs
-sed -i -e "s/\"homepage\": .*$/\"homepage\": \"\\.\",/g" package.json
+sed -i -e "s/\"start_url\": \"\\.\"/\"start_url\": \"\\/react-para\\/\"/g" build/client/manifest.json
+mv build/client docs
+#sed -i -e "s/\"homepage\": .*$/\"homepage\": \"\\.\",/g" package.json
 git add -A && git commit -am "updated demo" && git push -f origin master
 echo "-- done --"
